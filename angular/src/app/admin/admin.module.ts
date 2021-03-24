d68b3a167000fcdff7b0f24ec4d7e3093f1fec21
Nhantran4125 <tranthithanhnhan4125@gmail.com>
2021-03-24 14:57:26 +0700

    Tao components cho trang Admin

--- angular/src/app/admin/admin.module.ts
+++ angular/src/app/admin/admin.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule,routingAdminComponents } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AsidebarComponent } from './asidebar/asidebar.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, SidebarComponent, TopbarComponent, FooterComponent, AsidebarComponent],
   imports: [
     CommonModule,
     AdminRoutingModule

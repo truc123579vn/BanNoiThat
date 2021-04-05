16b940d6023474b0e19a88d0e068d95096fbb176
NHAnhIT <78128687+NHAnhIT@users.noreply.github.com>
2021-04-05 13:21:52 +0700

    Update admin-routing.module.ts

--- angular/src/app/admin/admin-routing.module.ts
+++ angular/src/app/admin/admin-routing.module.ts
@@ -1,27 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AdminComponent} from './admin.component';
+import { AdminComponent } from './admin.component';
 import { LoginComponent } from './login/login.component';
-import { ProductsComponent } from './products/products.component';
+
+// khai bao class de dan link "vinh"
+import { ProductsComponent } from '../admin/products/products.component';
+import { AccountComponent } from '../admin/account/account.component';
+import { OrdersComponent } from '../admin/orders/orders.component';
+import { StatisticsComponent } from '../admin/statistics/statistics.component';
 
 const routes: Routes = [
-  {path:'',component:LoginComponent ,
+  {
+    path: '',
+    component: LoginComponent,
     // children: [
     //   {path: 'products',component:ProductsComponent},
     // ]
   },
-  {path:"index", component:AdminComponent,
+  {
+    path: 'index',
+    component: AdminComponent,
     children: [
-      {path: 'products',component:ProductsComponent},
-    ]
+      { path: 'products', component: ProductsComponent },
+      { path: 'accounts', component: AccountComponent }, // khai baso path"vinh"
+      { path: 'orders', component: OrdersComponent},
+      { path: 'statistics', component: StatisticsComponent},
+      
+      // {path:'login',component:LoginComponent}
+    ],
   },
-  {path:'', redirectTo: '/admin', pathMatch:'prefix'},
+  { path: '', redirectTo: '/admin', pathMatch: 'prefix' },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AdminRoutingModule { }
-export const routingAdminComponents=[ProductsComponent]
+export class AdminRoutingModule {}
+export const routingAdminComponents = [ProductsComponent];

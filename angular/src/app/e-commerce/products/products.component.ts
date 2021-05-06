8905b55260b5864908cd25bbef2d92dfa178e561
truc123579vn <tranchanhtruc123@gmail.com>
2021-05-06 13:23:15 +0700

    Commit Hide Scrollball ngang

--- angular/src/app/e-commerce/products/products.component.ts
+++ angular/src/app/e-commerce/products/products.component.ts
@@ -2,37 +2,43 @@ import { productModel } from 'src/app/models/product.model';
 import { ProductsService } from './../../services/products.service';
 import { Component, OnInit } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
-
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  page: number = 1;
   listProduct!:productModel[]; 
   featureProduct!:productModel[];
-
+  slides !:productModel[]
   constructor(private service:ProductsService) {
     this.service.getProduct().subscribe(
       data =>{
-        this.listProduct = data;
+        this.listProduct  = data
 
       }
     )
   
    }
 
-  slides: any = [[]];
-  chunk(arr : any, chunkSize: any ) {
-    let R = [];
-    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
-      R.push(arr.slice(i, i + chunkSize));
-    }
-    return R;
-  }
+  //  cards =  this.service.getProduct().subscribe(
+  //   data =>{
+  //     this.listProduct  = data
+
+  //   }
+  // )
+
+
+  // chunk(arr : any, chunkSize: any ) {
+  //   let R = [];
+  //   for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+  //     R.push(arr.slice(i, i + chunkSize));
+  //   }
+  //   return R;
+  // }
   ngOnInit(): void {
-    this.slides = this.chunk(this.listProduct, 3);
+    // this.slides = this.chunk(this.listProduct, 3);
 
   }
   // images = ['../../assets/img/slider-1.jpg','../../assets/img/slider-2.jpg','../../assets/img/slider-3.jpg'];
